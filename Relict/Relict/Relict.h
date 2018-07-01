aedecceb083b21f9324157ae3dba31f1b58e4bb5
jowkame <>
2018-07-01 19:58:55 +0300

    Add base project

---
+++ Relict/Relict/Relict.h
@@ -0,0 +1,19 @@
+//
+//  Relict.h
+//  Relict
+//
+//  Created by jowkame on 7/1/18.
+//  Copyright © 2018 jowkame. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Relict.
+FOUNDATION_EXPORT double RelictVersionNumber;
+
+//! Project version string for Relict.
+FOUNDATION_EXPORT const unsigned char RelictVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Relict/PublicHeader.h>
+
+
